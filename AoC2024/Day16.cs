f4961192964ed2c6bf5199e5a506216755489a0e
Eduard Lepner <eduard@invarion.com>
2024-12-16 18:17:47 +0100

    complete day 16 pt2

--- AoC2024/Day16.cs
+++ AoC2024/Day16.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -37,53 +38,85 @@ public class Day16
     }
 
     [Fact]
+    public void ShouldSolveSamplePt2()
+    {
+        Assert.Equal(45, SolvePt2(Sample));
+    }
+
+    [Fact]
     public void ShouldSolvePt1()
     {
         var result = Solve(File.ReadAllText("TestAssets/day16.txt"));
         Assert.Equal(99488, result);
     }
 
+    [Fact]
+    public void ShouldSolvePt2()
+    {
+        var result = SolvePt2(File.ReadAllText("TestAssets/day16.txt"));
+        Assert.Equal(516, result);
+    }
+
     public int Solve(string input)
     {
         var maze = ParseInput(input);
         var visited = Solve(maze);
-        var result = visited.Where(x => x.Key.Location == maze.Finish).MinBy(x => x.Value).Value;
-        return result;
+
+        
+
+        var result = visited.Where(x => x.Key.Location == maze.Finish).MinBy(x => x.Value.Cost).Value;
+        return result.Cost;
     }
 
-    Dictionary<((int, int) Location, Direction Direction), int> Solve(Maze maze)
+    public int SolvePt2(string input)
+    {
+        var maze = ParseInput(input);
+        var visited = Solve(maze);
+
+        var min = visited.Where(x => x.Key.Location == maze.Finish).MinBy(x => x.Value.Cost);
+        var result = CountSteps(visited, min.Key);
+
+        return result.Count;
+    }
+
+    Dictionary<Vertex, (int Cost, Vertex[] From)> Solve(Maze maze)
     {
 
-        var front = new Dictionary<Vertex, int>();
-        front[(maze.Start, Direction.E)] = 0;
-        var visited = new Dictionary<Vertex, int>(); ;
+        var front = new Dictionary<Vertex, (int Cost, Vertex[] From)>();
+        front[(maze.Start, Direction.E)] = (0, []);
+        var visited = new Dictionary<Vertex, (int Cost, Vertex[] From)>();
         while (front.Count > 0)
         {
             //front.TryGetValue(front.Min, out var value);
             // var (vertex, cost) = front.Min;
             // front.Remove(front.Min);
-            var current = front.MinBy(x => x.Value);
+            var current = front.MinBy(x => x.Value.Cost);
             var vertex = current.Key;
-            var cost = current.Value;
+            var costInfo = current.Value;
             front.Remove(vertex);
-            visited.Add(vertex, cost);
+            visited.Add(vertex, costInfo);
             foreach (var neighbor in GetNeighbours(maze, vertex))
             {
-                var currentCost = cost + neighbor.Cost;
+                var currentCost = costInfo.Cost + neighbor.Cost;
 
                 if (visited.ContainsKey(neighbor.Vertex))
                 {
                     continue;
                 }
 
-                if (front.TryGetValue(neighbor.Vertex, out var existingCost))
+                if (front.TryGetValue(neighbor.Vertex, out var existingCostInfo))
                 {
-                    if (existingCost > currentCost)
-                        front[neighbor.Vertex] = currentCost;
+                    if (existingCostInfo.Cost > currentCost)
+                    {
+                        front[neighbor.Vertex] = (currentCost, [vertex]);
+                    } else if (existingCostInfo.Cost == currentCost)
+                    {
+                        front[neighbor.Vertex] = (currentCost, [.. existingCostInfo.From, vertex]);
+                    }
                 }
                 else
                 {
-                    front[neighbor.Vertex] = currentCost;
+                    front[neighbor.Vertex] = (currentCost, [vertex]);
                 }
             }
 
@@ -112,6 +145,17 @@ public class Day16
 
 
         return directionsWithCost;
+    }
+
+    private HashSet<(int, int)> CountSteps(Dictionary<Vertex, (int Cost, Vertex[] From)> pathMap, Vertex current, HashSet<(int, int)> visited = null!)
+    {
+        visited ??= new HashSet<(int, int)>();
+        visited.Add(current.Location);
+        foreach (var tuple in pathMap[current].From)
+        {
+            CountSteps(pathMap, tuple, visited);
+        }
+        return visited;
     }
 
     private Maze ParseInput(string input)
